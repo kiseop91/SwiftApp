e682ca847f01c0029d6589b280e972b1dfb38a71
김기섭 <kiseop@gimgiseob-ui-MacBookPro.local>
2023-07-15 14:52:55 +0900

    Add app Badges

---
+++ SwiftEx/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,26 @@
+//
+//  RotatedBadgeSymbol.swift
+//  SwiftEx
+//
+//  Created by 김기섭 on 2023/07/15.
+//
+
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
